137dd1fd6f845946ced712ece54830ac5c693b41
yousef <yousefkamel282@gmail.com>
2023-08-27 12:49:53 +0300

    finishing the server file

---
+++ server.py
@@ -0,0 +1,26 @@
+import socket
+
+# local ip address
+Host = socket.gethostbyname(socket.gethostname())
+Port = 9000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.bind((Host, Port))
+
+server.listen(5)  # listen for connections with maximum of 5 connections in queue
+print(f'Listening on IP: {Host}  and Port: {Port}...') 
+
+packet_number = 0
+while True:
+    data, client_address = server.recvfrom(1024)
+    msg = data.decode('utf-8')
+
+    if msg == "exit":
+        break
+
+    packet_to_send = f"Packet {packet_number}".encode()
+    server.sendto(packet_to_send, client_address)
+    print(f"Sent packet {packet_number} to {client_address}")
+    packet_number += 1
+
+server.close()        

735ab132da5e867a1372804a29b6fea3d79c0b62
Karim Abou Gharbia <karim91_@hotmail.com>
2023-08-27 16:52:18 +0300

    some minor modifications such as having stop cond.

--- client.py
+++ client.py
@@ -1,31 +1,38 @@
 import socket as sk
-import time
 
-IPaddress = "197.160.225.16"
+# setting IP and port numbers
+IPaddress = "192.168.1.108"
 port = 12345
 
-packet = 0
-
-
+# client connects with server
 sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
 sock.connect((IPaddress, port))
 
+# input taken from user
+exit_condition = 0
+while exit_condition < 1:
+    exit_condition = int(input("Stop at packet number? "))
 
-
-
-while True:
-    time.sleep(0.5)
-    
+# loop sends to server for new packets and receives those new packets
+packet = 0
+while packet <= exit_condition:
     print(f"Sent packet {packet}")
-    
-    packet=str(packet)
 
-    sock.sendto(packet.encode('utf-8'), (IPaddress, port))
+    packet = str(packet)
 
+    # data is encoded then sent over to server
+    sock.sendto(packet.encode("utf-8"), (IPaddress, port))
 
     data = sock.recv(1024)
 
+    # data is decoded and displayed
     packet = data.decode()
     print(f"Received packet #{packet}")
+
     packet = int(packet)
-    packet+=1
+
+    # checks for stop condition to send to server
+    if packet == exit_condition:
+        data = "exit"
+        sock.sendto(data.encode("utf-8"), (IPaddress, port))
+    packet += 1
--- server.py
+++ server.py
@@ -4,23 +4,28 @@ import socket
 Host = socket.gethostbyname(socket.gethostname())
 Port = 12345
 
+# initialize server
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind((Host, Port))
 
-print(f'Listening on IP: {Host}  and Port: {Port}...') 
+print(f"Listening on IP: {Host}  and Port: {Port}...")
 
+# loop checks for requests from client
 packet_number = 0
 while True:
+    # client's request is received and decoded
     data, client_address = server.recvfrom(1024)
-    msg = data.decode('utf-8')
+    msg = data.decode("utf-8")
 
+    # if data sent meets stop condition then loop breaks
     if msg == "exit":
         break
-    packet_number=int(msg)
+    packet_number = int(msg)
 
-    packet_to_send = f"{packet_number}".encode('utf-8')
+    # packet is encoded and sent to client
+    packet_to_send = f"{packet_number}".encode("utf-8")
     server.sendto(packet_to_send, client_address)
     print(f"Sent packet {packet_number} to {client_address}")
-  
 
-server.close()        
+# if loop breaks the server closes
+server.close()
